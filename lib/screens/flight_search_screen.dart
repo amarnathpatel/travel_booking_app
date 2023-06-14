a2e3a2ba197f5167522e3e5bcc28da2c1a2122ea
Amar Nath <amanath@cisco.com>
2023-06-14 16:30:18 +0530

    Input validation on Flight Search screen

--- lib/screens/flight_search_screen.dart
+++ lib/screens/flight_search_screen.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
 
+import '../models/flight.dart';
 import 'flight_results_list_screen.dart';
 
-class FlightSearchScreen extends StatelessWidget {
+class FlightSearchScreen extends StatefulWidget {
   static const String routeName = '/flight';
 
   const FlightSearchScreen({super.key});
 
   @override
+  State<FlightSearchScreen> createState() => _FlightSearchScreenState();
+}
+
+class _FlightSearchScreenState extends State<FlightSearchScreen> {
+  TextEditingController sourceAirportInputController = TextEditingController();
+  TextEditingController destinationAirportInputController =
+      TextEditingController();
+  TextEditingController passengersInputController = TextEditingController();
+  TextEditingController departureDateInputController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
   Widget build(BuildContext context) {
     return Form(
+      key: _formKey,
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
@@ -17,27 +32,57 @@ class FlightSearchScreen extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.fromLTRB(0.0, 0.0, 64.0, 8.0),
               child: TextFormField(
+                controller: sourceAirportInputController,
+                keyboardType: TextInputType.name,
+                validator: (value) {
+                  debugPrint('Departure airport  input Validator : $value');
+                  if (value!.isEmpty) {
+                    return 'Departure airport can\'t be blank';
+                  } else {
+                    return null;
+                  }
+                },
                 decoration: const InputDecoration(
                   icon: Icon(Icons.flight_takeoff, color: Colors.red),
-                  labelText: "From",
+                  labelText: "Departure Airport Code",
                 ),
               ),
             ),
             Padding(
               padding: const EdgeInsets.fromLTRB(0.0, 0.0, 64.0, 8.0),
               child: TextFormField(
+                controller: destinationAirportInputController,
+                keyboardType: TextInputType.name,
+                validator: (value) {
+                  debugPrint('Destination airport input Validator : $value');
+                  if (value!.isEmpty) {
+                    return 'Destination airport can\'t be blank';
+                  } else {
+                    return null;
+                  }
+                },
                 decoration: const InputDecoration(
                   icon: Icon(Icons.flight_land, color: Colors.red),
-                  labelText: "To",
+                  labelText: "Destination Airport Code",
                 ),
               ),
             ),
             Padding(
               padding: const EdgeInsets.fromLTRB(0.0, 0.0, 64.0, 8.0),
               child: TextFormField(
+                controller: passengersInputController,
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  debugPrint('Passengers input Validator : $value');
+                  if (value!.isEmpty) {
+                    return 'Passengers field can\'t be blank';
+                  } else {
+                    return null;
+                  }
+                },
                 decoration: const InputDecoration(
                   icon: Icon(Icons.person, color: Colors.red),
-                  labelText: "Passengers",
+                  labelText: "Number of Passengers",
                 ),
               ),
             ),
@@ -51,35 +96,87 @@ class FlightSearchScreen extends StatelessWidget {
                   child: Padding(
                     padding: const EdgeInsets.only(right: 16.0),
                     child: TextFormField(
+                      controller: departureDateInputController,
+                      keyboardType: TextInputType.datetime,
                       decoration: const InputDecoration(
-                        labelText: 'Departure',
+                        labelText: 'Departure Data',
                         hintText: 'Please input date of departure.',
                       ),
+                      validator: (value) {
+                        debugPrint('Departure date  input Validator : $value');
+                        if (value!.isEmpty) {
+                          return 'Departure date field can\'t be blank';
+                        } else {
+                          return null;
+                        }
+                      },
                     ),
                   ),
                 ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 16.0),
-                    child: TextFormField(
-                      decoration: const InputDecoration(labelText: "Arrival"),
-                    ),
-                  ),
-                ),
+                // Expanded(
+                //   child: Padding(
+                //     padding: const EdgeInsets.only(left: 16.0),
+                //     child: TextFormField(
+                //       decoration: const InputDecoration(labelText: "Arrival"),
+                //     ),
+                //   ),
+                // ),
               ],
             ),
-            Expanded(
-                child: Center(
-                    child: ElevatedButton(
-                        onPressed: () {
-                          // TO DO Passing argumants
-                          Navigator.pushNamed(
-                              context, FlightResultsListScreen.routeName);
-                        },
-                        child: const Text('Search Flights')))),
+            // Expanded(
+            //   child: Center(
+            //     child: ElevatedButton(
+            //       onPressed: () {
+            //         _onSearchFlightBtnTap();
+            //       },
+            //       child: const Text('Search Flights'),
+            //     ),
+            //   ),
+            // ),
+            const SizedBox(height: 10.0),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              ElevatedButton(
+                onPressed: () {
+                  _onSearchFlightBtnTap();
+                },
+                child: const Text('Search Flights'),
+              ),
+              const SizedBox(width: 10.0),
+              ElevatedButton(
+                onPressed: () {
+                  _onReset();
+                },
+                child: const Text('Reset'),
+              ),
+            ])
           ],
         ),
       ),
     );
+  }
+
+  void _onSearchFlightBtnTap() {
+    if (_formKey.currentState!.validate()) {
+      String sourceAirportCode = sourceAirportInputController.text;
+      String destinationAirportCode = destinationAirportInputController.text;
+      String noOfPassengers = passengersInputController.text;
+      String departureDate = departureDateInputController.text;
+      debugPrint('Input data: source : $sourceAirportCode');
+      debugPrint('Input data: destination : $destinationAirportCode');
+      debugPrint('Input data: No of passengers : $noOfPassengers');
+      debugPrint('Input data: Departure Date : $departureDate');
+
+      // TO DO Passing argumants
+      Navigator.pushNamed(context, FlightResultsListScreen.routeName,
+          arguments:
+              Flight(DateTime(2023), DateTime(2023), '466', 'Air Ashia'));
+    }
+  }
+
+  _onReset() {
+    sourceAirportInputController.clear();
+    destinationAirportInputController.clear();
+    passengersInputController.clear();
+    departureDateInputController.clear();
   }
 }
